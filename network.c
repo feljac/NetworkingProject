2e4598a5c4d34206ef274d561d7aace42098af1e
feljac <felixjacoby@yahoo.fr>
2018-10-16 16:29:43 +0200

    networking functions and struct added

---
+++ network.c
@@ -0,0 +1,102 @@
+//
+// Created by root on 16/10/18.
+//
+
+#include "network.h"
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+
+const char * real_address(const char *address, struct sockaddr_in6 *rval){
+    struct addrinfo* hints = (struct addrinfo*) calloc(1, sizeof(struct addrinfo));
+    struct addrinfo* res;
+    hints->ai_family = AF_INET6;
+    hints->ai_socktype = SOCK_DGRAM;
+    hints->ai_protocol = 0;
+
+
+    if ((getaddrinfo(address, NULL, hints, &res)) != 0) {
+        return ("Getaddrinfo Error\n");
+    }
+
+    while(res){
+        if(res->ai_family == AF_INET6){
+            //rval = (struct sockaddr_in6*) res[0].ai_addr;
+            memcpy(rval, (struct sockaddr_in6*)res->ai_addr, sizeof(*rval));
+            rval->sin6_family = AF_INET6;
+        }
+        res = res->ai_next;
+        return NULL;
+    }
+    return "No address found";
+}
+
+int create_socket(struct sockaddr_in6 *source_addr, int src_port, struct sockaddr_in6 *dest_addr, int dst_port){
+    int fd;
+
+    if(!source_addr && !dest_addr){
+        return -1;
+    }
+
+    if((fd = socket(PF_INET6, SOCK_DGRAM, 0)) == -1) {
+        fprintf(stderr, "Socket Error");
+        return -1;
+    }
+
+    struct sockaddr_in6 address;
+
+    if(source_addr && src_port > 0){
+        memcpy(&address, source_addr, sizeof(address));
+        address.sin6_port = htons(src_port);
+        if(bind(fd, (struct sockaddr *) &address, sizeof(address)) == -1){
+            fprintf(stderr, "Bind Error%s\n", strerror(errno));
+            return -1;
+        }
+    }
+    else if(dest_addr && dst_port > 0) {
+        memcpy(&address, dest_addr, sizeof(address));
+        address.sin6_port = htons(dst_port);
+        if (connect(fd, (struct sockaddr*) &address, sizeof(address)) == -1){
+            fprintf(stderr, "Connect Error %s\n", strerror(errno));
+            return -1;
+        }
+    }
+    else{
+        return -1;
+    }
+
+    return fd;
+}
+
+int wait_for_client(int sfd){
+    char* buf[1024];
+    ssize_t nbrbytes;
+    struct sockaddr_storage* src_addr =(struct sockaddr_storage*) calloc(1, sizeof(struct sockaddr_storage));
+    socklen_t sendsize = sizeof(src_addr);
+
+    fprintf(stderr,"Server waiting\n");
+
+    if((nbrbytes = recvfrom(sfd, buf, sizeof(buf), MSG_PEEK, (struct sockaddr*) src_addr, &sendsize)) == -1){
+        fprintf(stderr, "Error wait recvfrom\n");
+        return -1;
+    }
+
+    fprintf(stderr,"Server connect...\n");
+
+    if(connect(sfd, (struct sockaddr*) src_addr, sendsize) == -1){
+        fprintf(stderr, "Error wait connect\n");
+        return -1;
+    }
+
+    fprintf(stderr,"Server connected\n");
+    return 0;
+}
---
+++ network.h
@@ -0,0 +1,58 @@
+//
+// Created by root on 16/10/18.
+//
+
+#ifndef NETWORKING_PROJECT_NETWORK_H
+#define NETWORKING_PROJECT_NETWORK_H
+
+#endif //NETWORKING_PROJECT_NETWORK_H
+
+#ifndef __CREATE_SOCKET_H_
+#define __CREATE_SOCKET_H_
+
+#include <netinet/in.h> /* * sockaddr_in6 */
+#include <sys/types.h> /* sockaddr_in6 */
+
+/* Creates a socket and initialize it
+ * @source_addr: if !NULL, the source address that should be bound to this socket
+ * @src_port: if >0, the port on which the socket is listening
+ * @dest_addr: if !NULL, the destination address to which the socket should send data
+ * @dst_port: if >0, the destination port to which the socket should be connected
+ * @return: a file descriptor number representing the socket,
+ *         or -1 in case of error (explanation will be printed on stderr)
+ */
+int create_socket(struct sockaddr_in6 *source_addr,
+                  int src_port,
+                  struct sockaddr_in6 *dest_addr,
+                  int dst_port);
+
+#endif
+
+#ifndef __REAL_ADDRESS_H_
+#define __REAL_ADDRESS_H_
+
+/* Resolve the resource name to an usable IPv6 address
+ * @address: The name to resolve
+ * @rval: Where the resulting IPv6 address descriptor should be stored
+ * @return: NULL if it succeeded, or a pointer towards
+ *          a string describing the error if any.
+ *          (const char* means the caller cannot modify or free the return value,
+ *           so do not use malloc!)
+ */
+const char * real_address(const char *address, struct sockaddr_in6 *rval);
+
+#endif
+
+#ifndef __WAIT_FOR_CLIENT_H_
+#define __WAIT_FOR_CLIENT_H_
+
+/* Block the caller until a message is received on sfd,
+ * and connect the socket to the source addresse of the received message.
+ * @sfd: a file descriptor to a bound socket but not yet connected
+ * @return: 0 in case of success, -1 otherwise
+ * @POST: This call is idempotent, it does not 'consume' the data of the message,
+ * and could be repeated several times blocking only at the first call.
+ */
+int wait_for_client(int sfd);
+
+#endif
