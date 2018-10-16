2054b2fac712742d4c0252dadd3fe6b0b7cdb916
Jimmy <jimmy.delacruz@student.uclouvain.be>
2018-10-16 17:21:10 +0200

    add makefile

--- receiver.c
+++ receiver.c
@@ -4,6 +4,6 @@
 
 #include "receiver.h"
 
-int main(int argc, char** argv){
+int main(){
     return 0;
 }
--- sender.c
+++ sender.c
@@ -4,7 +4,7 @@
 
 #include "sender.h"
 
-int main(int argc, char** argv){
+int main(){
 
     return 0;
 }
